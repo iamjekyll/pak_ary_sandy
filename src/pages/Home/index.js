066c85c41989a4dabac04991234882c64aa9e23a
iamjekyll <cinowini@gmail.com>
2021-01-15 19:37:26 +0700

    change description

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { Header, Info, Product } from "../../components";
-import "./Home.css";
 
 const Homepage = () => {
   return (
@@ -8,18 +7,6 @@ const Homepage = () => {
       <Header />
       <Info />
       <Product />
-      <div className="homepage__wrapper">
-        <div className="homepage__container masthead">
-          <div className="homepage__textcont">
-            <h1>Satu platform terbaik untuk bisnis yang semakin berkembang.</h1>
-            <p>
-              Mulai fokus pada perkembangan bisnis Anda, Cumagini membantu Anda
-              untuk otomasi Marketing anda secara online.
-            </p>
-            <button>Pelajari Tentang Cumagini</button>
-          </div>
-        </div>
-      </div>
     </div>
   );
 };
