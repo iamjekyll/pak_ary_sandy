a2e41693dea3e6903e83c9228bf0e8de5f43d589
iamjekyll <cinowini@gmail.com>
2021-01-21 19:36:26 +0700

    hapus link card

--- src/atoms/Card/index.js
+++ src/atoms/Card/index.js
@@ -1,26 +1,23 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
+import React from "react";
 
 const Card = (props) => {
-    const {name, price1, price2, img} = props;
-    return (
-        <div className="pro__ctn">
-            <Link to={name}>
-                <div className="pro__bx">
-                    <img src={img} alt={name} />
-                    <h3>{name}</h3>
-                    <div className="pro_ptp">
-                        <p>6 Bulan</p>
-                        <p className="pro__harga">Rp {price1}</p>
-                    </div>
-                    <div className="pro_pbt">
-                        <p>12 Bulan</p>
-                        <p className="pro__harga">Rp {price2}</p>
-                    </div>
-                </div>
-            </Link>
+  const { name, price1, price2, img } = props;
+  return (
+    <div className="pro__ctn">
+      <div className="pro__bx">
+        <img src={img} alt={name} />
+        <h3>{name}</h3>
+        <div className="pro_ptp">
+          <p>6 Bulan</p>
+          <p className="pro__harga">Rp {price1}</p>
         </div>
-    )
-}
+        <div className="pro_pbt">
+          <p>12 Bulan</p>
+          <p className="pro__harga">Rp {price2}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Card
+export default Card;
